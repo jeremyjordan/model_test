8c5aaa08eb0ff43bf11ced10df7f6423406355cf
Jeremy Jordan <jtjordan@ncsu.edu>
2020-09-27 11:49:57 -0400

    improve module import

--- model_test/discovery.py
+++ model_test/discovery.py
@@ -1,7 +1,8 @@
 """
 Discover all test cases to generate.
 """
-from importlib import import_module
+import importlib
+import sys
 from inspect import getmembers, isfunction
 from pathlib import Path
 from typing import Callable, List, Tuple, Union
@@ -17,6 +18,29 @@ def find_test_modules(dir_path: str, prefix: str = "test", suffix: str = None) -
     return list(path.rglob(search_str))
 
 
+def robust_module_import(module_path: Path):
+    """
+    From: pytest/pathlib.py:import_path
+    """
+    module_name = module_path.stem
+
+    for meta_importer in sys.meta_path:
+        if not hasattr(meta_importer, "find_spec"):
+            continue
+        spec = meta_importer.find_spec(module_name, [str(module_path.parent)])
+        if spec is not None:
+            break
+        else:
+            spec = importlib.util.spec_from_file_location(module_name, str(module_path))
+
+    if spec is None:
+        raise ImportError(
+            "Can't find module {} at location {}".format(module_name, str(module_path))
+        )
+    module = importlib.util.module_from_spec(spec)
+    return module
+
+
 def find_test_functions(
     module_path: Union[str, Path], prefix: str = "test", suffix: str = None
 ) -> List[Tuple[str, Callable]]:
@@ -26,7 +50,7 @@ def find_test_functions(
         suffix = ""
 
     module_path = str(module_path).replace("/", ".").strip(".py")
-    module = import_module(module_path)
+    module = robust_module_import(module_path)
     functions_list = [
         (name, func)
         for name, func in getmembers(module)
